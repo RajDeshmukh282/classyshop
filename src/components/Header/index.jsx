cb29b6ad178b6a21c26080fd54e5b244b04ce6e7
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-06-11 21:30:50 +0530

    Fix typo in className attribute and update promotional text in Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <div classname="top-strip py-2">
+      <div className="top-strip py-2">
         <div className=" container">
           <div className="flex items-center justify-between">
             <div className="col1 w-[50%]">
               <p className="text-[14px]">
-                get upto 50% off on diwali sell hurry up!!
+                Shop now and get up to 50% off during our Diwali Sale! Limited time offer!
               </p>
             </div>
           </div>
