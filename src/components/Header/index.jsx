baa897b0f612934ba90df13b40a909bf4832f97b
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-06-11 21:50:13 +0530

    Refactor Header component and CSS for improved structure and consistency

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <div className="top-strip py-2 bg-primary">
+      <div className="top-strip py-2">
         <div className=" container">
           <div className="flex items-center justify-between">
             <div className="col1 w-[50%]">
-              <p className="text-[14px]">
+              <p className="text-[14px] font-[500]">
                 Shop now and get up to 50% off during our Diwali Sale! Limited time offer!
               </p>
             </div>
